3e4af3d0dbedeb543cacc89802733188aff7031e
vaibhav <60617102+vai4411@users.noreply.github.com>
2020-09-04 19:57:01 +0530

    [Vaibhav]ADD: services layer for park vehicle

--- ParkingLotBusinessLayer/ParkingService.cs
+++ ParkingLotBusinessLayer/ParkingService.cs
@@ -12,6 +12,25 @@ namespace ParkingLotBusinessLayer
     /// </summary>
     public class ParkingService : IParkingService
     {
+        private readonly IParkingRepository parkingRepository;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ParkingService"/> class.
+        /// </summary>
+        /// <param name="parkingRepository">Parking repository.</param>
+        public ParkingService(IParkingRepository parkingRepository)
+        {
+            this.parkingRepository = parkingRepository;
+        }
+
+        /// <summary>
+        /// This method used for for parking new vehicle in parking lot.
+        /// </summary>
+        /// <param name="parking">Parking data.</param>
+        /// <returns>Boolean result.</returns>
+        public bool ParkVehicle(Parking parking)
+        {
+            return this.parkingRepository.ParkVehicle(parking);
+        }
     }
 }
