67c9064453b988b725e9e6e6897cde5a4b4eb4a7
vaibhav <60617102+vai4411@users.noreply.github.com>
2020-09-04 19:58:18 +0530

    [Vaibhav]ADD: controller mapping for park vehicle

--- ParkingLot/Controllers/ParkingLotController.cs
+++ ParkingLot/Controllers/ParkingLotController.cs
@@ -17,6 +17,39 @@ namespace ParkingLot.Controllers
     [ApiController]
     public class ParkingLotController : ControllerBase
     {
-        
+        private readonly IParkingService parkingService;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ParkingLotController"/> class.
+        /// </summary>
+        /// <param name="parkingService">Parking service.</param>
+        public ParkingLotController(IParkingService parkingService)
+        {
+            this.parkingService = parkingService;
+        }
+
+        /// <summary>
+        /// This method used for park vehicle using post mapping.
+        /// </summary>
+        /// <param name="parking">Parking object.</param>
+        /// <returns>Action result.</returns>
+        [HttpPost]
+        public ActionResult ParkVehicle([FromBody] Parking parking)
+        {
+            try
+            {
+                bool result = this.parkingService.ParkVehicle(parking);
+                if (result == true)
+                {
+                    return this.Ok(new ResponseEntity(HttpStatusCode.OK, "Vehicle parked successfully", result));
+                }
+
+                return this.NotFound(new ResponseEntity(HttpStatusCode.NotFound, "Please check details again", result));
+            }
+            catch (Exception e)
+            {
+                return this.BadRequest(new { success = false, message = e.Message });
+            }
+        }
     }
 }
